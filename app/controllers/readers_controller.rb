4f6e9647c120a2c25431fa50ea325c9aa73484f2
Muhammad <muhammadhullah23@gmail.com>
2021-06-03 22:08:07 -0400

    commit tree 261dd72fc32d807ed0f9ba6a9229feccdda7147c

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -6,9 +6,16 @@ class ReadersController < ApplicationController
         erb :'readers/signup'
     end
     post '/signup' do
-        @reader = Reader.create(email: params[:email], username: params[:username], password: params[:password])
-        session[:reader_id] = @reader.id
-        redirect to '/readers'
+        binding.pry
+        puts params
+        if params[:email] || params[:username] || params[:password].empty?
+            redirect to '/signup'
+        else
+            @reader = Reader.create(email: params[:email], username: params[:username], password: params[:password])
+            session[:reader_id] = @reader.id
+            redirect to '/readers'
+        end
+       
     end
     
 end
