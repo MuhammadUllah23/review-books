75e2be31746de347b3e82523b551a3781c2e1f1b
Muhammad <muhammadhullah23@gmail.com>
2021-06-09 03:14:24 -0400

    post route to build review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,9 +4,14 @@ class ReviewsController < ApplicationController
         @reviews = Review.all
         erb :'book-reviews/index'
     end
+    
     get '/reviews/new' do
-        
         erb :'book-reviews/new'
+    end
+
+    post '/review' do
+        @review = current_user.movies.build(params)
+        redirect to '/review/:id'
     end
     
 end
