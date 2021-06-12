3a49041dcfad1d40ce0eb195f6cccd631693da1f
Muhammad <muhammadhullah23@gmail.com>
2021-06-11 20:29:35 -0400

    edit users form. can only edit username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,8 @@ class UsersController < ApplicationController
         redirect_if_not_logged_in
         @user = User.find(params[:id])
         redirect_if_not_authorized
-        @user.update
+        @user.update(params[:user])
+        redirect to "/account/#{@user.id}"
     end
 
     get '/account/:id' do
