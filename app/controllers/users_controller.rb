14aa19e1be5725e822032357c4a834d9f2de6311
Muhammad <muhammadhullah23@gmail.com>
2021-06-14 17:19:24 -0400

    All pages and routes work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect to '/readers'
+            redirect to '/'
         else
             redirect to '/login'
         end  
